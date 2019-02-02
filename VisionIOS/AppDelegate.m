c7acef85c84daa2b01a2607c60ea2907eb6339fa
侯华生 <331292108@qq.com>
2019-02-02 23:41:42 +0800

    音频播放和posix信号量测试

--- VisionIOS/AppDelegate.m
+++ VisionIOS/AppDelegate.m
@@ -15,6 +15,10 @@
 #import "UIApplication+RunLoop.h"
 #import "HHGcdController.h"
 #import "HHSortController.h"
+#import "HHAudioController.h"
+
+#import "pthread.h"
+#import "semaphore.h"
 
 @interface AppDelegate ()
 
@@ -22,12 +26,59 @@
 
 @implementation AppDelegate
 
+sem_t *sem;
+
+void *operate(void *param) {
+//    NSString *str = (__bridge NSString *)(param);
+    // 用循环模拟耗时操作
+//    for (int i = 0; i < 100000; i++) {
+//        // [NSThread currentThread] 为获取当前
+//        NSLog(@"timeConsuming in %@, times: %d, param: %@", [NSThread currentThread], i, str);
+//    }
+    sem_wait(sem);
+    NSLog(@"first thread try!\n");
+    sleep(5);
+    NSLog(@"first thread post!\n");
+    sem_post(sem);
+    pthread_exit((void*)0);
+}
+
+void *secondPthread(void *param) {
+    //    NSString *str = (__bridge NSString *)(param);
+    // 用循环模拟耗时操作
+    //    for (int i = 0; i < 100000; i++) {
+    //        // [NSThread currentThread] 为获取当前
+    //        NSLog(@"timeConsuming in %@, times: %d, param: %@", [NSThread currentThread], i, str);
+    //    }
+    sem_wait(sem);
+    NSLog(@"second thread waked!\n");
+    sleep(5);
+    sem_post(sem);
+    pthread_exit((void*)0);
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     [self.window makeKeyAndVisible];
 
-    self.window.rootViewController = [[HHScrollViewController alloc] init];
+    self.window.rootViewController = [[HHAudioController alloc] init];
+    
+    pthread_t thread1;
+    pthread_t thread2;
+    
+//    sem = sem_open("/test", O_CREAT, O_RDWR, 1);
+    sem = sem_open("name_sem", O_CREAT|O_RDWR, 0666, 1);
+    
+    pthread_create(&thread1, NULL, operate, NULL);
+    pthread_create(&thread2, NULL, secondPthread, NULL);
+    
+//    for (int i = 0; i < 500; i ++) {
+//
+//        pthread_t pthread;
+//        pthread_create(&pthread, NULL, operate, NULL);
+//    }
+//    self.window.rootViewController = [[HHScrollViewController alloc] init];
 //    self.window.rootViewController = [[HHGcdController alloc] init];
     
 //    [self addObserver];
